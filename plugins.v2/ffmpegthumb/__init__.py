bd5280f5568fa3fba61f748ce1ca36b75ab5f7ab
leo <504861978@qq.com>
2025-01-17 13:18:20 +0800

    fix(ffmpegthumb): v2.2

--- plugins.v2/ffmpegthumb/__init__.py
+++ plugins.v2/ffmpegthumb/__init__.py
@@ -28,7 +28,7 @@ class FFmpegThumb(_PluginBase):
     # 插件图标
     plugin_icon = "ffmpeg.png"
     # 插件版本
-    plugin_version = "2.1"
+    plugin_version = "2.2"
     # 插件作者
     plugin_author = "jxxghp"
     # 作者主页
